89a48fa60059b7f900b73cb034d2195cc21f2b05
Johnathan Driscoll <jdriscoll@cpp.edu>
2022-10-23 14:54:06 -0700

    added numGuessGame.c

---
+++ Question3/numGuessGame.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int maxNUm = 10;
+
+int main(void){
+    int menuOpt = 3;
+    int imput;
+    int number;
+    char menuRtn;
+    time_t t;
+    srand((unsigned) time(&t));
+
+    while (menuOpt < 4 && menuOpt)
+
+}
