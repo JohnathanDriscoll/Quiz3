db553202581b29f283489833ade74e4c6c08a5bb
Johnathan Driscoll <jdriscoll@cpp.edu>
2022-10-23 13:57:53 -0700

    Question3 Initial commit, added gameMenu.h

---
+++ Question3/gameMenu.h
@@ -0,0 +1,4 @@
+//functions defined in gameMenu.c
+extern void playGame(void);  //input type is void, and return type is void
+extern void changeMaxNum(void);
+extern void quitGame(void);
--- employee.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stddef.h>
-typedef struct {
-    long number;        //4 bytes
-    char *name;         //8 bytes
-    char *phone;        //8 bytes
-    double salary;      //8 bytes
-} Employee, *PtrToEmployee;
-typedef const Employee *PtrToConstEmployee;
