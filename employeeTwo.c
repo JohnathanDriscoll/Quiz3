683c0f3e4e4ca87d8e5c36c8f3d6b40e26f9ff75
Johnathan Driscoll <jdriscoll@cpp.edu>
2022-10-22 17:36:42 -0700

    removed employeeOne.c changed employeeTwo.c created example of search failure

--- employeeTwo.c
+++ employeeTwo.c
@@ -29,6 +29,14 @@ static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableVa
 {
     return strcmp((char *) targetPtr, tableValuePtr->name);//const void *targetPtr ==> typecast as char pointer then pass into strcmp()
 }
+static int compareEmployeePhone(const void *targetPtr, PtrToConstEmployee tableValuePtr)
+{
+    return strcmp((char *) targetPtr, tableValuePtr->phone);//const void *targetPtr ==> typecast as char pointer then pass into strcmp()
+}
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
+{
+    return * (double *) targetPtr != tableValuePtr->salary; //const void *targetPtr ==> typecast as int pointer then dereference
+}
 
 
 
@@ -40,4 +48,12 @@ PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long numb
 PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char* name)
 {
     return searchEmployeeTable(ptr, size, name, compareEmployeeName);
+}
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char* phone)
+{
+    return searchEmployeeTable(ptr, size, phone, compareEmployeePhone);
+}
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary)
+{
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeSalary);
 }
